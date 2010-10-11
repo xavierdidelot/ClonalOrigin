cafb858e247d9e4877674eb4f8f00ebb56f69b34
aaron.darling <aaron.darling@88ae0664-b54d-9b4c-f12c-25a0e96714a6>
2010-10-11 06:03:46 +0000

    script to split up xmfa alignment on blocks

---
+++ warg/scripts/blocksplit.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w 
+use strict;
+
+my $fname = $ARGV[0];
+
+open(INFILE, "$fname") || die "Unable to open input XMFA file $fname\n";
+my $i = 1;
+my $curoutfile = $fname.".1";
+open(OUTFILE, ">$curoutfile");
+while( my $line = <INFILE> )
+{
+	print OUTFILE $line;
+	if($line =~ /=/)
+	{
+		close OUTFILE;
+		$i++;
+		$curoutfile = $fname.".$i";
+		open(OUTFILE, ">$curoutfile");
+	}
+}
+close OUTFILE;
